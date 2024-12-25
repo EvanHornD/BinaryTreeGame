080b755a8b32ac60d114691454001cc49225c27a
Evan Horn <ehorn01@yahoo.com>
2024-12-25 14:51:11 -0700

    started work on the console input, found the raw console input class online

---
+++ Lab9_Horn/src/game/UserInput/TerminalInput.java
@@ -0,0 +1,33 @@
+package game.UserInput;
+
+public class TerminalInput {
+
+        // try {
+        //     while (true) {
+        //         int keyCode = RawConsoleInput.read(false); // Wait for a key press
+        //         if (keyCode == -1) {
+        //             System.out.println("End of input (EOF).");
+        //             break;
+        //         }
+        //         System.out.println("Key pressed: " + keyCode + " (" + (char) keyCode + ")");
+
+        //         if ((char) keyCode == 'q') {
+        //             System.out.println("Exiting...");
+        //             break;
+        //         }
+        //         Thread.sleep(33);
+        //     }
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // } finally {
+        //     try {
+        //         RawConsoleInput.resetConsoleMode(); // Reset console mode to normal
+        //     } catch (Exception e) {
+        //         System.err.println("Failed to reset console mode.");
+        //         e.printStackTrace();
+        //     }
+        // }
+
+}
+
+
